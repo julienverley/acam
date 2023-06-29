d0f0e3ceae02ed12de711a44d623b4ccaed8cfb4
Julien Verley <mjules@gmail.com>
2023-06-29 14:58:58 +0200

    X fermeture menu mobile

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -25,6 +27,7 @@ const Navbar = () => {
           height={100}
         />
       </Link>
+      {/* Desktop menu */}
       <div className="hidden lg:block">
         <ul className="flex items-center">
           <Link href="/rejoindre">
@@ -71,42 +74,25 @@ const Navbar = () => {
       </div>
       <div className="block lg:hidden">
         <button
-          className="p-2 text-gray-800"
+          className="p-2 text-gray-800 relative z-50"
           onClick={toggleMenu}
           aria-label="Toggle Menu"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
+          <span className="z-30">
             {isMenuOpen ? (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
+              <CloseIcon style={{ fontSize: "2rem" }} />
             ) : (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
+              <MenuIcon style={{ fontSize: "2rem" }} />
             )}
-          </svg>
+          </span>
         </button>
+        {/* Mobile menu */}
         <div
           className={`${
             isMenuOpen ? "block" : "hidden"
-            // } absolute top-0 left-0 w-full h-screen bg-white`}
           } absolute top-0 left-0 w-full h-fit bg-yellow-300 z-20`}
         >
-          {/* <ul className="flex flex-col items-center pt-24"> */}
-          <ul className="flex flex-col items-center pt-6">
+          <ul className="flex flex-col items-center pt-10">
             <Link href="/">
               <button
                 className="p-5 m-1 text-black hover:bg-zinc-800 hover:text-white"
@@ -147,7 +133,6 @@ const Navbar = () => {
                 Souvenirs
               </li>
             </Link>
-            {/* <Link href="/jartdins"> */}
             <a target="_blank" href="https://www.lesjartdinsdemontagny.fr/">
               <li
                 className="p-5 m-1 hover:bg-zinc-800 hover:text-white"
@@ -156,13 +141,6 @@ const Navbar = () => {
                 JARTdins
               </li>{" "}
             </a>
-            {/* <li
-                className="p-5 m-1 hover:bg-zinc-800 hover:text-white"
-                onClick={handleMenuItemClick}
-              >
-                JARTdins
-              </li> */}
-            {/* </Link> */}
           </ul>
         </div>
       </div>
